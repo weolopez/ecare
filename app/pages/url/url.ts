336168206ae68a80af1fdf1ffbd95b7a976eb26e
Lopez, Mauricio (ml5174) <ml5174@att.com>
2016-05-11 18:01:57 -0400

    removed localhost reference

--- app/pages/url/url.ts
+++ app/pages/url/url.ts
@@ -5,7 +5,7 @@ import {Iframe} from '../../components/iframe/iframe';
   Generated class for the IphonePage page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.http://localhost:8100/?article=1500001197
+  Ionic pages and navigation.?article=1500001197
 */
 @Page({
   templateUrl: 'build/pages/url/url.html',
@@ -13,35 +13,35 @@ import {Iframe} from '../../components/iframe/iframe';
 })
 export class URL {
   winwidth: string='320px';
-  url: string="http://localhost:8100/?video=1500001197";
+  url: string="?video=1500001197";
   urllist: Array<any>=[
     {
       name: "Set up Visual Voicemail",
-      url: "http://localhost:8100/?article=1500001197"
+      url: "?article=1500001197"
     },
     {
       name: "Insert SIM card",
-      url: "http://localhost:8100/?article=1500001346"
+      url: "?article=1500001346"
     },
     {
       name: "Block calls",
-      url: "http://localhost:8100/?article=1500001360"
+      url: "?article=1500001360"
     },
     {
       name: "Keyboard & typing",
-      url: "http://localhost:8100/?article=1500001362"
+      url: "?article=1500001362"
     },
     {
       name: "Personal hotspot",
-      url: "http://localhost:8100/?article=1500001373"
+      url: "?article=1500001373"
     },
     {
       name: "View & manage data",
-      url: "http://localhost:8100/?article=1500001423"
+      url: "?article=1500001423"
     },
     {
       name: "Bluetooth ",
-      url: "http://localhost:8100/?article=1500004587"
+      url: "?article=1500004587"
     }
   ]
   constructor(public nav: NavController) {}
